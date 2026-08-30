a36dc5732ed1c654379f43eb3d080ea347c60107
Junhan <191029948+ZachL1113@users.noreply.github.com>
2026-08-30 23:09:10 +0800

    Rename chatbot and add greeting and exit.

---
+++ src/main/java/Nova.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Nova {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Nova.");
+        System.out.println("What can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye! See you next time.");
+                break;
+            }
+        }
+    }
+}
