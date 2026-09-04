354aa8c2e0c3e456fa1d3ada387431eae4678e2e
ZachL1113 <E1481840@u.nus.edu>
2026-09-04 11:41:07 +0800

    Add support for adding and listing tasks

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Nova {
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Nova.");
         System.out.println("What can I do for you?");
 
+        List<String> tasks = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
@@ -12,8 +17,20 @@ public class Nova {
             if (input.equals("bye")) {
                 System.out.println("Bye! See you next time.");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(HORIZONTAL_LINE);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println(HORIZONTAL_LINE);
             } else {
-                System.out.println(input);
+                tasks.add(input);
+                System.out.println(HORIZONTAL_LINE);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + input);
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                System.out.println(HORIZONTAL_LINE);
             }
         }
     }
